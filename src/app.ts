b311e5c9893c73163a89331719fafaa88661ce09
anyi arfuso <anyiarfuso35@gmail.com>
2024-06-27 20:28:32 -0300

    Subiendo crud certificado

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { cursoRouter } from './curso/cursos.routes.js';
 import { alumnoRouter } from './alumno/alumnos.routes.js';
+import { certificadoRouter } from './certificado/certificado.routes.js';
 
 const app = express() 
 app.use(express.json()) 
 
 app.use("/api/cursos", cursoRouter)
 app.use("/api/alumnos", alumnoRouter)
+app.use('/api/certificados', certificadoRouter)
 
 app.listen (3000, ()=>{
     console.log("Server running on http: //localhost:3000/")
